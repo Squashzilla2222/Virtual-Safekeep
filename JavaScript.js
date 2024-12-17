32f5f5018a3aa64c615009911b616c66f015bcc1
Willow Senior <146185442+Squashzilla2222@users.noreply.github.com>
2024-12-17 16:35:05 +0000

    Start of code
    The HTML and CSS look fine for the first page but it does not work (of course)

---
+++ JavaScript.js
@@ -0,0 +1,27 @@
+﻿function change_content(page) {
+
+    switch (page) {
+        case "register_account":
+            document.getElementById("content").innerHTML = "Register new account";
+            break;
+        case "home":
+            document.getElementById("content").innerHTML = "Welcome to the home page";
+            break;
+        case "password_storage":
+            document.getElementById("content").innerHTML = " Welcome to the details storage page";
+            break;
+        case "password_strength":
+            document.getElementById("content").innerHTML = "Welcome to the password testing page";
+            break;
+        case "account_options":
+            document.getElementById("content").innerHTML = "Account options";
+            break;
+        default:
+            document.getElementById("content").innerHTML = "Page not found";
+    }
+}
+
+document.getElementById("login").addEventListener("submit", function (event) {
+    event.preventDefault();
+    change_content("home");
+});
