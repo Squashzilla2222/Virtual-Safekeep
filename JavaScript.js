a87eba1e1d8286e5d4a045715f9fada01d6ba47d
Willow Senior <146185442+Squashzilla2222@users.noreply.github.com>
2024-12-23 22:01:12 +0000

    Big JS change
    Working SPA

--- JavaScript.js
+++ JavaScript.js
@@ -1,29 +1,8 @@
-﻿function change_content(page) {
+function login() {
+    event.preventDefault();
+    var login_page = document.getElementById("login_page");
+    var home = document.getElementById("home");
 
-    switch (page) {
-        case "register_account":
-            document.getElementById("content").innerHTML = "Register new account";
-            break;
-        case "home":
-            document.getElementById("content").innerHTML = "Welcome to the home page";
-            break;
-        case "password_storage":
-            document.getElementById("content").innerHTML = " Welcome to the details storage page";
-            break;
-        case "password_strength":
-            document.getElementById("content").innerHTML = "Welcome to the password testing page";
-            break;
-        case "account_options":
-            document.getElementById("content").innerHTML = "Account options";
-            break;
-        default:
-            document.getElementById("content").innerHTML = "Page not found";
-    }
+    login_page.style.display = "none";
+    home.style.display = "block";
 }
-
-document.getElementById("login").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents form from reloading
-    document.querySelector(".main").style.display = "none"; // Hide login
-    document.getElementById("content").style.display = "block";
-    change_content("home");
-});
