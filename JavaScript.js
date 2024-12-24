cfc9cf270e87375090c533e70132de3e591f29c4
Willow Senior <146185442+Squashzilla2222@users.noreply.github.com>
2024-12-24 15:37:16 +0000

    VS not working
    updating JS

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,31 @@
-function login() {
-    event.preventDefault();
-    var login_page = document.getElementById("login_page");
-    var home = document.getElementById("home");
+var login_page = document.getElementById("login_page");
+var home = document.getElementById("home");
+var manage_acc = document.getElementById("manage_accounts")
+var password_test = document.getElementById("password_tester")
+var acc_options = document.getElementById("account_options")
+var quit = document.getElementById("quit")
 
-    login_page.style.display = "none";
-    home.style.display = "block";
+function login() {
+    event.preventDefault(); // prevent login page form reloading
+    login_page.style.display = "none"; // hide login
+    home.style.display = "block"; // show home page
+}
+
+function manage_accounts() {
+    home.style.display = "none";
+    manage_acc.style.display = "block";
+}
+
+function password_strength() {
+    home.style.display = "none";
+    password_test.style.display = "block";
+}
+
+function account_options() {
+    home.style.display = "none";
+    acc_options.style.display = "block";
+}
+
+function quit() {
+    window.close();
 }
