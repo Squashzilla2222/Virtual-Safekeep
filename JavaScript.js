f1d4629688b8b8ec7db40d1b8dced4e44fcd6b50
Willow Senior <146185442+Squashzilla2222@users.noreply.github.com>
2024-12-24 16:20:23 +0000

    SPA application done
    kinda messy on this side, especially with the back_to_home function, but it works

--- JavaScript.js
+++ JavaScript.js
@@ -1,31 +1,45 @@
-var login_page = document.getElementById("login_page");
-var home = document.getElementById("home");
-var manage_acc = document.getElementById("manage_accounts")
-var password_test = document.getElementById("password_tester")
-var acc_options = document.getElementById("account_options")
-var quit = document.getElementById("quit")
-
 function login() {
     event.preventDefault(); // prevent login page form reloading
+    var login_page = document.getElementById("login_page");
+    var home = document.getElementById("home");
+
     login_page.style.display = "none"; // hide login
     home.style.display = "block"; // show home page
 }
 
 function manage_accounts() {
+    var home = document.getElementById("home");
+    var manage_acc = document.getElementById("manage_accounts")
     home.style.display = "none";
     manage_acc.style.display = "block";
 }
 
 function password_strength() {
+    var home = document.getElementById("home");
+    var password_test = document.getElementById("password_tester")
     home.style.display = "none";
     password_test.style.display = "block";
 }
 
 function account_options() {
+    var home = document.getElementById("home");
+    var acc_options = document.getElementById("account_options")
     home.style.display = "none";
     acc_options.style.display = "block";
 }
 
 function quit() {
     window.close();
+}
+
+function back_to_home() {
+    var home = document.getElementById("home");
+    var manage_acc = document.getElementById("manage_accounts")
+    var password_test = document.getElementById("password_tester")
+    var acc_options = document.getElementById("account_options")
+
+    manage_acc.style.display = "none";
+    password_test.style.display = "none";
+    acc_options.style.display = "none";
+    home.style.display = "block";
 }
